a2b0ff57d6ae72ab6d805a85c4850c562d211982
1egend <51487023+1e9end@users.noreply.github.com>
2020-12-07 22:29:32 -0500

    Update main.js

--- betterbrawlers/main.js
+++ betterbrawlers/main.js
@@ -1,6 +1,7 @@
 var clubInfo;
 var url= "https://api.brawlstars.com/v1/clubs/%2328GYUQJ9Q/members";//https://api.brawlstars.com/v1/clubs/%2328GYUQJ9Q/members";
 var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjljNDE1NmI4LTcxMzgtNGNmOS04ODNlLTIyZGJjZmVlYWY3MiIsImlhdCI6MTYwNzM4MjEzOCwic3ViIjoiZGV2ZWxvcGVyLzQ1NDQ2MzUzLWJmMmEtNzcyZC0xN2I4LTJhNjY1ODk3NzhiNCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTYyLjIyMi44MC4yNDAiXSwidHlwZSI6ImNsaWVudCJ9XX0.aB6ruGD-Ve-BXyvPq_p0WVU3xfptbIOsvqW0L2GflxwrKfgEvUJaLb1b59Emcv8wLhQSKjbFnFjoNJKo1J7Hzg";
+/**
 $.ajax({
     url: url,
     type: "GET",
@@ -16,8 +17,7 @@ $.ajax({
     	console.log(clubInfo);
     }
 });
-
-/**
+**/
 fetch(url, {
   mode: "cors",
   method: "GET",
@@ -33,7 +33,6 @@ console.log(data);
 .catch(function(error) {
 console.log(error);
 });
-**/
  
 function p(name, a, b, c, d, e, f, g){
 	var sum = a + b + c + d + e + f + g;
