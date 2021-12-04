177aad5afdb67bf423b1a4c42d630091039cdb30
1egend <Charles.ran9@gmail.com>
2021-12-04 14:21:31 -0500

    Updated website

--- main.js
+++ main.js
@@ -1,132 +1,127 @@
-// Initialize Swiper 
-const swiper = new Swiper('.swiper-container', {
+/**
+// Initialize Swiper
+const swiper = new Swiper(".swiper-container", {
     autoplay: {
         delay: 4000,
-        disableOnInteraction: false
+        disableOnInteraction: false,
     },
-    direction: 'horizontal',
+    direction: "horizontal",
     loop: true,
     centeredSlides: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     slidesPerView: 1,
     spaceBetween: 20,
     breakpoints: {
         1500: {
-            slidesPerView: 3
+            slidesPerView: 3,
         },
         1250: {
-            slidesPerView: 2
-        } 
+            slidesPerView: 2,
+        },
     },
-    effect: 'coverflow'
+    effect: "coverflow",
 });
+
 
 // Initialize tsParticles
 tsParticles.load("bg", {
-    "particles": {
-        "number": {
-        "value": 40,
-        "density": {
-            "enable": true,
-            "value_area": 700
-        }
+    particles: {
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 700,
+            },
         },
-        "color": {
-            "value": "#AA0000"
+        color: {
+            value: "#AA0000",
         },
-        "shape": {
-        "type": "circle",
-        "stroke": {
-            "width": 0,
-            "color": "#000000"
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000",
+            },
+            polygon: {
+                nb_sides: 5,
+            },
         },
-        "polygon": {
-            "nb_sides": 5
+        opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 0.1,
+                opacity_min: 0.1,
+                sync: false,
+            },
         },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 10,
+                size_min: 0.1,
+                sync: false,
+            },
         },
-        "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-            "enable": false,
-            "speed": 0.1,
-            "opacity_min": 0.1,
-            "sync": false
-        }
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#000000",
+            opacity: 0.8,
+            width: 1,
         },
-        "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-            "enable": false,
-            "speed": 10,
-            "size_min": 0.1,
-            "sync": false
-        }
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+            },
         },
-        "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#000000",
-        "opacity": 0.4,
-        "width": 1
-        },
-        "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-        }
-        }
     },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-        "onhover": {
-            "enable": true,
-            "mode": "grab"
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            resize: true,
         },
-        "onclick": {
-            "enable": true,
-            "mode": "repulse"
+        modes: {
+            grab: {
+                distance: 140,
+                line_linked: {
+                    opacity: 1,
+                },
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3,
+            },
+            repulse: {
+                distance: 200,
+                duration: 1,
+            },
+            push: {
+                particles_nb: 4,
+            },
+            remove: {
+                particles_nb: 2,
+            },
         },
-        "resize": true
-        },
-        "modes": {
-        "grab": {
-            "distance": 140,
-            "line_linked": {
-            "opacity": 1
-            }
-        },
-        "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
-        },
-        "repulse": {
-            "distance": 200,
-            "duration": 1
-        },
-        "push": {
-            "particles_nb": 4
-        },
-        "remove": {
-            "particles_nb": 2
-        }
-        }
     },
-    "retina_detect": true
+    retina_detect: true,
 });
+**/
